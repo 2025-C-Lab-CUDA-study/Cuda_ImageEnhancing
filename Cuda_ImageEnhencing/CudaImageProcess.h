b0ab29e693a6bba5a0e874f88905e6ce87f80869
JJM <108610835+didvkahs@users.noreply.github.com>
2026-03-19 20:02:25 +0900

    CUDA unsharp mask

---
+++ Cuda_ImageEnhencing/CudaImageProcess.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "typedef.h"
+
+class CudaImageProcess
+{
+public:
+
+	CudaImageProcess(void);
+	~CudaImageProcess(void);
+
+	bool Initialize(void);
+	bool ImageBlur(void);
+	bool IncreaseResolution(void);
+	bool StoreImage(void);
+	void Close(void);
+
+	bool BilinearReduce(void);
+	bool BilinearIncrease(void);
+
+private:
+
+	bool applyKawase(uint32_t applyTimes);
+
+private:
+
+	Buf_s* h_rBuf;
+	Buf_s* h_gBuf;
+	Buf_s* h_bBuf;
+	uint32_t width, height;
+
+	uint8_t* d_originalR;
+	uint8_t* d_originalG;
+	uint8_t* d_originalB;
+
+	uint8_t* d_rBuf;
+	uint8_t* d_gBuf;
+	uint8_t* d_bBuf;
+
+	uint8_t* d_r2; 
+	uint8_t* d_g2;
+	uint8_t* d_b2;
+
+	size_t pitch;
+
+};
---
+++ Cuda_ImageEnhencing/main.cu
@@ -0,0 +1,55 @@
+#include<cuda_runtime.h>
+#include<device_launch_parameters.h>
+
+#include <iostream>
+#include <cstdint>
+
+#include "CudaImageProcess.h"
+
+inline void CudaCheck(cudaError_t err)
+{
+    if (err != cudaSuccess)
+    {
+        std::cerr << "CUDA Error : " << cudaGetErrorString(err) << "at line " << __LINE__ <<"\n";
+        exit(EXIT_FAILURE);
+    }
+}
+
+inline void CudaKernelCheck(void)
+{
+    CudaCheck(cudaGetLastError());
+}
+
+int main(void)
+{
+    CudaImageProcess imageProcessor;
+
+    if (!imageProcessor.Initialize())
+    {
+        fprintf(stderr, "imageProcessor initialize failed\n");
+        return 1;
+    }
+
+    if (!imageProcessor.ImageBlur())
+    {
+        fprintf(stderr, "imageProcessor imageBlur failed\n");
+        imageProcessor.Close();
+        return 1;
+    }
+    if (!imageProcessor.IncreaseResolution())
+    {
+        fprintf(stderr, "imageprocessor increaseresolution failed\n");
+        imageProcessor.Close();
+        return 1;
+    }
+
+    if (!imageProcessor.StoreImage())
+    {
+        fprintf(stderr, "imageProcessor StoreImage failed\n");
+        imageProcessor.Close();
+        return 1;
+    }
+    imageProcessor.Close();    
+
+    return 0;
+}
